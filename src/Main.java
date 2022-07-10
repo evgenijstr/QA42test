de7860cd26001dbb043a49bdb47afc48fc70551c
Evgenijstr <evgenijstr@gmail.com>
2022-07-10 23:28:23 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2+3");
+        System.out.println(2+3);
+
+        int speed;
+        speed = 17;
+        int x = speed + 3;
+
+        int price = 280;
+
+        double d = 6.5;
+        String name = "Zhenya";
+
+        System.out.println(speed);
+        System.out.println(price);
+
+        System.out.println(name + " ехал со скоростью " + speed);
+    }
+}
